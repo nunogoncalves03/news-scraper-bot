a7e6c1f1e5edad341cf781283f29e062e3dc29ee
Nuno Gonçalves <93784204+nunogoncalves03@users.noreply.github.com>
2022-02-16 21:03:59 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+import discord
+import os
+import requests
+from bs4 import BeautifulSoup
+from discord.ext import tasks
+from keep_alive import keep_alive
+import datetime
+from replit import db
+from news_scraper import *
+from discord_components import Select, SelectOption, ComponentsBot
+from discord.utils import get
+from functools import reduce
+
+
+intents = discord.Intents.default()
+intents.members = True
+bot = ComponentsBot('!', intents=intents)
+
+
+async def select_role(ctx):
+    await ctx.send(content='Escolhe os sites que pretendes receber notícias. <:icon:943544386367651850>', components=[Select(
+                              placeholder='Escolhe aqui!',
+                              min_values=1,
+                              max_values=6,
+                              options=[
+                                  SelectOption(label='Metropolitano de Lisboa', value='Metropolitano de Lisboa', emoji='🚇'),
+                                  SelectOption(label='Rodoviária de Lisboa', value='Rodoviária de Lisboa', emoji='🚌'),
+                                  SelectOption(label='CARRIS', value='CARRIS', emoji='🚎'),
+                                  SelectOption(label='Fertagus', value='Fertagus', emoji='🚊'),
+                                  SelectOption(label='TST', value='TST', emoji='🚌'),
+                                  SelectOption(label='Uniarea', value='Uniarea', emoji='📕')
+                              ],
+                              custom_id='SelectRole'
+    )])
+    while True:
+        interaction = await bot.wait_for('select_option', check=lambda inter: inter.custom_id == 'SelectRole')
+        values = interaction.values
+        author = interaction.author
+        for value in values:
+            role = get(author.guild.roles, name=value)
+            await author.add_roles(role)
+        await interaction.send('Vais receber notícias dos seguintes sites: ' + reduce(lambda x, y: x + ', ' + y, values) + '.')
+
+
+@bot.event
+async def on_ready():
+    await bot.wait_until_ready()
+    print(f'Logged in as {bot.user}!')
+    select_role_channel = bot.get_channel(943484725614481430)
+    logs_channel = bot.get_channel(922530769883246642)
+    await logs_channel.send(f'Logged in as {bot.user}! <:icon:943544386367651850> @everyone')
+    find_news.start()
+    await select_role_channel.purge(limit=1)
+    await select_role(select_role_channel)
+
+
+@bot.event
+async def on_member_join(member):
+    guild = bot.get_guild(922444030720245810)
+    channel = guild.get_channel(943597491167821894)
+    await channel.send(f'Bem-vindo ao servidor, {member.mention}! Vai ao <#943484725614481430> para escolheres os sites que pretendes receber notícias. <:icon:943544386367651850>')
+
+
+@tasks.loop(seconds = 3600)
+async def find_news():
+    logs_channel = bot.get_channel(922530769883246642)
+    await logs_channel.send(f'Searching for news... <:icon:943544386367651850>')
+    for news in find_all_news():
+        if news[0] != db['last_message']:
+            embed = discord.Embed(title=news[0], url=news[1], color=discord.Color.blue())
+            embed.set_author(name=news[2], icon_url=news[3])
+            date_time = datetime.datetime.now().strftime("%d/%m/%Y")
+            embed.set_footer(text=f'News Scraper • {date_time}')
+            channel = bot.get_channel(news[4])
+            role = get(channel.guild.roles, name=news[2])
+            await channel.send(f'Nova notícia de **{news[2]}** {role.mention}\n', embed=embed)
+            db['last_message'] = news[0]
+    for error in find_errors():
+        await logs_channel.send(f'**{error[0]}** @everyone\n**Error:** {error[1]}')
+        reset_error_list()
+    await logs_channel.send('Last message: **' + db['last_message'] + '**')
+    print('Last message:', db['last_message'])
+
+
+keep_alive()
+bot.run(os.environ['token'])
---
+++ news_scraper.py
@@ -0,0 +1,317 @@
+import requests
+from bs4 import BeautifulSoup
+from replit import db
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+error_list = []
+
+
+def write_all_rl_news():
+    html_text = requests.get('https://www.rodoviariadelisboa.pt/comunicados').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('a', class_='titulo')
+    for post in posts:
+        post_ref = 'https://www.rodoviariadelisboa.pt/' + post['href']
+        db[post_ref] = ''
+    print('write_all_rl_news() ✅')
+
+
+def find_rl_news():
+  try:
+    html_text = requests.get('https://www.rodoviariadelisboa.pt/comunicados').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('a', class_='titulo')
+    additional_news = []
+    for post in posts:
+        post_title = post.text.strip()
+        post_ref = 'https://www.rodoviariadelisboa.pt/' + post['href']
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'Rodoviária de Lisboa', 'https://i.ibb.co/848PYh9/rl.png', 943596787476877336]]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_rl_news() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_rl_news() ❌', error] not in error_list:
+        error_list = error_list + [['find_rl_news() ❌', error]]
+    print('find_rl_news() ❌')
+    return []
+
+
+def write_all_c_news():
+    html_text = requests.get('https://www.carris.pt/descubra/noticias/informacoes-de-servico/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('div', class_='container widget')
+    for post in posts:
+        post_ref = post.find('div', class_='col-12 col-md-6 image-block order-md-1').a['href']
+        if post_ref[0:19] == '/descubra/noticias/' or post_ref[0:6] == '/viaje':
+            post_ref = 'https://www.carris.pt' + post_ref
+        db[post_ref] = ''
+    print('write_all_c_news() ✅')
+
+
+def find_c_news():
+  try:
+    html_text = requests.get('https://www.carris.pt/descubra/noticias/informacoes-de-servico/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('div', class_='container widget')
+    additional_news = []
+    for post in posts:
+        post_ref = post.find('div', class_='col-12 col-md-6 image-block order-md-1').a['href']
+        if post_ref[0:19] == '/descubra/noticias/' or post_ref[0:6] == '/viaje':
+            post_ref = 'https://www.carris.pt' + post_ref
+        post_title = post.find('div', class_='subtitle left').text.strip()        
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'CARRIS', 'https://i.ibb.co/hB0npQ1/carris.png', 943596807739551844]]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_c_news() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_c_news() ❌', error] not in error_list:
+        error_list = error_list + [['find_c_news() ❌', error]]
+    print('find_c_news() ❌')
+    return []
+
+
+def write_all_ml_news_1():
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    html_text = requests.get('https://www.metrolisboa.pt/informar-3/noticias/', headers=headers).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2', class_='entry-title')
+    for post in posts:
+        post_ref = post.a['href']
+        db[post_ref] = ''
+    print('write_all_ml_news_1() ✅')
+
+
+def find_ml_news_1():
+  try:
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    html_text = requests.get('https://www.metrolisboa.pt/informar-3/noticias/', headers=headers).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2', class_='entry-title')
+    additional_news = []
+    for post in posts:
+        post_title = post.a.text.strip()
+        post_ref = post.a['href']
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'Metropolitano de Lisboa', 'https://i.ibb.co/bQctjmf/metro.png', 943596738135097344]]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_ml_news_1() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_ml_news_1() ❌', error] not in error_list:
+        error_list = error_list + [['find_ml_news_1() ❌', error]]
+    print('find_ml_news_1() ❌')
+    return []
+
+
+def write_all_ml_news_2():
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    html_text = requests.get('https://www.metrolisboa.pt/institucional/comunicar/comunicados/', headers=headers).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2', class_='entry-title')
+    for post in posts:
+        post_ref = post.a['href']
+        db[post_ref] = ''
+    print('write_all_ml_news_2() ✅')
+
+
+def find_ml_news_2():
+  try:
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    html_text = requests.get('https://www.metrolisboa.pt/institucional/comunicar/comunicados/', headers=headers).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2', class_='entry-title')
+    additional_news = []
+    for post in posts:
+        post_title = post.a.text.strip()
+        post_ref = post.a['href']
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'Metropolitano de Lisboa', 'https://i.ibb.co/bQctjmf/metro.png', 943596738135097344]]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_ml_news_2() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_ml_news_2() ❌', error] not in error_list:
+        error_list = error_list + [['find_ml_news_2() ❌', error]]
+    print('find_ml_news_2() ❌')
+    return []
+
+
+def write_all_uniarea_news():
+    html_text = requests.get('https://uniarea.com/category/noticias/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2', class_='title cb-post-title')
+    for post in posts:
+        post_ref = post.a['href']
+        db[post_ref] = ''
+    print('write_all_rl_news() ✅')
+
+
+def find_uniarea_news():
+  try:
+    html_text = requests.get('https://uniarea.com/category/noticias/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2', class_='title cb-post-title')
+    additional_news = []
+    for post in posts:
+        post_title = post.a.text.strip()
+        post_ref = post.a['href']
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'Uniarea', 'https://i.ibb.co/KVtT4Fs/uniarea.png', 943596874965872670]]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_uniarea_news() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_uniarea_news() ❌', error] not in error_list:
+        error_list = error_list + [['find_uniarea_news() ❌', error]]
+    print('find_uniarea_news() ❌')
+    return []
+
+
+def write_all_tst_news():
+    html_text = requests.get('https://www.tsuldotejo.pt/index.php?page=noticias').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2')
+    for post in posts:
+        post_ref = 'https://www.tsuldotejo.pt/' + post.a['href']
+        db[post_ref] = ''
+    print('write_all_tst_news() ✅')
+
+
+def find_tst_news():
+  try:
+    html_text = requests.get('https://www.tsuldotejo.pt/index.php?page=noticias').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2')
+    additional_news = []
+    for post in posts:
+        post_title = post.a.text.strip()
+        post_ref = 'https://www.tsuldotejo.pt/' + post.a['href']
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'TST', 'https://i.ibb.co/K0X2JTk/tst.png', 943596845878345788]]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_tst_news() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_tst_news() ❌', error] not in error_list:
+        error_list = error_list + [['find_tst_news() ❌', error]]
+    print('find_tst_news() ❌')
+    return []
+
+
+def find_fertagus_news():
+  try:
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('https://www.fertagus.pt/Fertagus-pt/Viajar/Comunicados-e-Campanhas')
+    additional_news = []
+    sleep(5)
+    for n in range(2, 6):
+        try:
+            post = driver.find_element(By.XPATH, f'//*[@id="divPages"]/div[{n}]/div[2]/div[2]/div/h2/strong/span')
+            post_title = post.get_attribute("innerText")
+            post_img = driver.find_element(By.XPATH, f'//*[@id="divPages"]/div[{n}]/div[2]/div[2]/a')
+            post_ref = post_img.get_attribute("href")
+            if post_ref not in db.keys():
+                print(f'{post_title}\n{post_ref}')
+                print()
+                additional_news += [[post_title, post_ref, 'Fertagus', 'https://i.ibb.co/3SNHFCk/fertagus.png', 943596830036467742]]
+        except:
+            pass
+    driver.quit()
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('find_fertagus_news() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['find_fertagus_news() ❌', error] not in error_list:
+        error_list = error_list + [['find_fertagus_news() ❌', error]]
+    print('find_fertagus_news() ❌')
+    return []
+
+
+def error_test():
+  try:
+    html_text = requests.get('https://www.fewfefowpjefifmklewmfkwemfweiofmewiçojwifwiefpio.com/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    posts = soup.find_all('h2')
+    additional_news = []
+    for post in posts:
+        post_title = post.a.text.strip()
+        post_ref = 'https://www.tsuldotejo.pt/' + post.a['href']
+        if post_ref not in db.keys():
+            print(f'{post_title}\n{post_ref}')
+            print()
+            additional_news += [[post_title, post_ref, 'Transportes Sul do Tejo', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNaXVTLTNR8R-wjKCMisxvm0uX5lGtqVXLg2k66z36igXHMH4lMMksXzwIeAdxij2zeaY&usqp=CAU']]
+    if additional_news != []:
+        for news in additional_news:
+            db[news[1]] = ''
+    print('error_test() ✅')
+    return additional_news
+  except Exception as error:
+    global error_list
+    if ['error_test() ❌', error] not in error_list:
+        error_list = error_list + [['error_test() ❌', error]]
+    print('error_test() ❌')
+    return []
+
+
+def write_all_news():
+    write_all_ml_news_1()
+    write_all_ml_news_2()
+    write_all_rl_news()
+    write_all_c_news()
+    write_all_uniarea_news()
+    write_all_tst_news()
+
+def find_errors():
+    global error_list
+    return error_list
+
+
+def reset_error_list():
+    global error_list
+    error_list = []
+
+
+def find_all_news():
+    return find_ml_news_1() + find_ml_news_2() + find_rl_news() + find_c_news() + find_uniarea_news() + find_tst_news() + find_fertagus_news()
