50d6c25cc5ad09a47da832c87436b38993cd442f
Nuno Gonçalves <nunomrgoncalves03@gmail.com>
2022-03-02 22:40:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import wget
 try:
     os.remove('news_scraper.py')
     news_scraper_file = wget.download('https://raw.githubusercontent.com/nunogoncalves03/news-scraper-bot/main/news_scraper.py')
+    print(f'\n{news_scraper_file} ✅')
     os.remove('discord_bot.py')
     discord_bot_file = wget.download('https://raw.githubusercontent.com/nunogoncalves03/news-scraper-bot/main/discord_bot.py')
+    print(f'\n{discord_bot_file} ✅')
 except:
-    print('news_scraper.py ❌\ndiscord_bot.py ❌')
+    print('news_scraper.py/discord_bot.py ❌')
 
 os.system('python redirect_to_main.py')
